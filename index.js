85842c7c09f55fac9d352547d67b433c1a0f4dc8
Byeongjae Kang <kangbj@pm.me>
2023-07-09 15:20:57 +0900

    .

--- index.js
+++ index.js
@@ -1,38 +1,95 @@
 const { ArgumentParser } = require('argparse')
-const fs = require('node:fs/promises')
+const { SerialPort } = require('serialport')
 
 const PROG_NAME = 'luxctl'
 const VERSION = '1.0.0'
-const BUILD_DATE = '5 July 2023'
+const BUILD_DATE = '7 July 2023'
 const AUTHOR = 'Byeongjae Kang'
+const DAC_RESOLUTION_BIT = 8
+const DIMMER = process.env.DIMMER
 
-const parser = new ArgumentParser({prog: PROG_NAME, description: 'Control brightness of monitor ambient light'})
+const dimmer = new SerialPort({ path: DIMMER, baudRate: 115200 })
+
+const parser = new ArgumentParser({ prog: PROG_NAME, description: 'Control brightness of monitor ambient light.', epilog: 'all brightness value on luxctl is a percentage number.' })
 const group = parser.add_mutually_exclusive_group()
 
-group.add_argument('-s', { metavar: 'N', type: 'int', nargs: 1,
-                            help: 'an integer which be set as brightness value.' })
-group.add_argument('-se', { metavar: 'N', type: 'int', nargs: 1,
-                            help: 'write new initial brightness value to EEPROM.'})
-group.add_argument('-r', { action: 'store_true', help: 'read current brightness value from dimmer.' })
-group.add_argument('-re', { action: 'store_true', help: 'read current initial brightness value from EEPROM.' })
+group.add_argument('-s', {
+    metavar: 'N', type: 'int', nargs: 1,
+    help: 'set a new brightness value.'
+})
+group.add_argument('-se', {
+    metavar: 'N', type: 'int', nargs: 1,
+    help: 'write new initial brightness to EEPROM.'
+})
+group.add_argument('-r', { action: 'store_true', help: 'read current brightness from dimmer.' })
+group.add_argument('-re', { action: 'store_true', help: 'read current initial brightness from EEPROM.' })
 group.add_argument('-v', { action: 'version', version: `${PROG_NAME} ${VERSION} of ${BUILD_DATE}, by ${AUTHOR}` })
 
-const args = parser.parse_args()
+let args = parser.parse_args()
 
-async function serialRW(cmd) {
-    const device = await fs.open(process.env.AMBIENT_LIGHT, 'a')
-        .then(fulfilled => fulfilled)
-        .catch(rejected => false)
-    //if(!device) throw new Error('Error occured during opening device.')
+const dacVal = [args.s, args.se].map(v => perc2raw(v))
 
-    const result = await device.appendFile(cmd)
-        .then(() => {
+function raw2perc(raw) { return Math.round(raw / ((2 ** DAC_RESOLUTION_BIT - 1) / 100)) }
+
+function perc2raw(perc) { return Math.round(perc * ((2 ** DAC_RESOLUTION_BIT - 1) / 100)) }
+
+function sendCmd(cmd, arg, isReturn = false) {
+    dimmer.write(`${cmd}${arg || ''}\n`)
+    dimmer.on('data', (data) => {
+        const response = data.toString().split("\r\n")[0]
+
+        if (response == 'OK') {
             return true
-        })
+
+        } else if (isReturn) {
+            let raw = parseInt(data.toString().split("\r\n")[0])
+            let perc = raw2perc(raw)
+
+            return { raw: raw, perc: perc }
+        }
+    })
 }
 
-if(args.s) {
-    const s = serialRW('CTRL' + args.s + '\n')
-} else if(args.se) {
-    const s = serialRW('SAVE' + args.se + '\n')
+async function processArgs() {
+    if (args.s) {
+        const res = await sendCmd('CTRL', perc2raw(args.s))
+            .then(() => true)
+            .catch(() => false)
+
+        if (res) {
+            console.log(`💡 brightness has been set to ${args.s}%. (PWM ${perc2raw(args.s)})`)
+        } else {
+            console.error('something went wrong.')
+        }
+        process.exit()
+
+    } else if (args.se) {
+        dimmer.write(`SAVE${dacVal[1]}\n`)
+        confirmDeviceResp(`💾 power-on brightness value on EEPROM has been set to ${args.se}%. (PWM ${dacVal})`)
+
+    } else if (args.r) {
+        const writeRes = await dimmer.write("READ\n")
+        dimmer.on('data', (data) => {
+            const raw = parseInt(data.toString().split("\r\n")[0])
+            const perc = raw2perc(raw)
+
+            console.log(`💡 current brightness is ${perc}%. (PWM ${raw})`)
+            process.exit();
+        });
+
+    } else if (args.re) {
+        await dimmer.write("RROM\n")
+        dimmer.on('data', (data) => {
+            const raw = parseInt(data.toString().split("\r\n")[0])
+            const perc = raw2perc(raw)
+
+            console.log(`🔍 current power-on brightness is ${perc}%. (PWM ${raw})`)
+            process.exit();
+        });
+
+    } else {
+        parser.print_help()
+        process.exit()
+    }
 }
+processArgs()
